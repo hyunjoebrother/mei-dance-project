98e002820deef426f7fc5a0e57e2023daee6b790
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-03-17 15:17:06 +0900

    feat: implement Instagram API && generate token logics

--- src/app/mei/page.tsx
+++ src/app/mei/page.tsx
@@ -1,29 +1,101 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import "../globals.css";
 
-export default function App() {
-  if (typeof window !== "undefined") {
-    const code = new URL(window.location.href).searchParams.get("code");
-  }
-  const clientCode = "347672391592808";
-  const redirectUri = "https://mei-dance-project.pages.dev/mei";
+interface InstagramPost {
+  id: string;
+  media_url: string;
+  permalink: string;
+}
 
-  const url = `https://api.instagram.com/oauth/authorize
-  ?client_id=${clientCode}
-  &redirect_uri=${redirectUri}
-  &scope=user_profile,user_media
-  &response_type=code
-`;
+const App: React.FC = () => {
+  const [posts, setPosts] = useState<InstagramPost[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const accessToken: string =
+          "IGQWRNVXBiLTM0MnQzWGpESkdPMnBYYVA2cFlxTk5VWGlCcFN1bHhrWkh1bnhiMnU3MHBiaTJvUmRJS0o3dnk1SUpxSFdZAVFRUeWRkWVdYN2lHYWxqTDhxNnZAYdHJsUVFycXoxYTJIbk5zOHJqZAzRwZAkpxcjBHbTNEWUp0Wm1zNXlIdwZDZD"; // 토큰 주기적으로 업데이트 필요
+        const userId: string = "7036437786478855";
+
+        const response = await fetch(
+          `https://graph.instagram.com/${userId}/media?fields=id,media_url,permalink&access_token=${accessToken}`
+        );
+        const data = await response.json();
+        console.log("data:", data);
+        setPosts(data.data);
+        console.log("data.data:", data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching Instagram posts:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className="font-bold  flex flex-col">
+    <div className="font-bold flex flex-col">
       온보딩 첫번째 페이지입니다
       <Link href="/mei/step">
         <button className="w-24 h-10 bg-red-500 rounded-2xl">
           다음 페이지로 이동
         </button>
       </Link>
-      <a href={url}>인스타그램 테스트</a>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {posts.map((post: InstagramPost) => (
+            // <div key={post.id}>
+            //   <img src={post.media_url} alt="Instagram Post" />
+            //   <a
+            //     href={post.permalink}
+            //     target="_blank"
+            //     rel="noopener noreferrer"
+            //   >
+            //     View on Instagram
+            //   </a>
+            // </div>
+            <div key={post.id}>
+              {post.permalink.includes("reel") ? ( // Check if the post is a reel
+                <div>
+                  <video
+                    src={post.media_url}
+                    controls
+                    width="300"
+                    height="300"
+                  ></video>{" "}
+                  {/* Render reel as video */}
+                  <a
+                    href={post.permalink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    릴스 확인하러 ㄱㄱㄱ
+                  </a>
+                </div>
+              ) : (
+                <div>
+                  <img src={post.media_url} alt="Instagram Post" />
+                  <a
+                    href={post.permalink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    게시글 확인하러 ㄱㄱㄱ
+                  </a>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default App;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
+//   const url = `https://api.instagram.com/oauth/authorize
+//   ?client_id=347672391592808
+//   &redirect_uri=https://mei-dance-project.pages.dev/auth
+//   &scope=user_profile,user_media
+//   &response_type=code
+// `;
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -8,6 +15,11 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
+        <Link href="/mei/step">
+          <button className="w-24 h-10 bg-red-500 rounded-2xl">
+            릴스 테스트 페이지!!!
+          </button>
+        </Link>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
